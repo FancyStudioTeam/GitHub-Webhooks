532803e60251df4e754388ec785c101693c3358f
JJSosaL <251519186+jjsosal@users.noreply.github.com>
2026-03-22 00:25:13 +0100

    refactor: `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,30 @@
-import { getInput, setFailed } from '@actions/core';
+import { getInput, info, setFailed } from '@actions/core';
 import { context } from '@actions/github';
-import type { ContainerBuilder } from '@discordjs/builders';
-import type { IssuesEvent } from '@octokit/webhooks-types';
-
-import { IssueClosedEventHandler } from './events/issues/IssueClosed.js';
-import { IssueOpenedEventHandler } from './events/issues/IssueOpened.js';
-import { PushEventHandler } from './events/Push.js';
+import { EventHandlersMap } from './lib/Handlers.js';
 import { WebhookClient } from './structures/WebhookClient.js';
 
-async function run(): Promise<void> {
+(async () => {
+	showContextData(context);
+
+	const { eventName, payload } = context;
+
+	const webhookId = getInput('webhook_id');
+	const webhookToken = getInput('webhook_token');
+
 	try {
-		const webhookId = getInput('webhook_id');
-		const webhookToken = getInput('webhook_token');
+		const webhookClient = new WebhookClient(webhookId, webhookToken);
+		const eventHandler = EventHandlersMap.get(eventName);
 
-		const webhook = new WebhookClient(webhookId, webhookToken);
-
-		const { eventName, payload } = context;
-
-		console.dir(context, {
-			colors: true,
-			depth: null,
-		});
-
-		switch (eventName) {
-			case 'issues': {
-				const { action } = payload as IssuesEvent;
-
-				const messages: IssueEventMessagesMap = {
-					closed: IssueClosedEventHandler.handle,
-					opened: IssueOpenedEventHandler.handle,
-				};
-				const message = messages[action];
-
-				if (message) {
-					await webhook.execute(message(payload as never));
-				}
-			}
-			case 'push': {
-				await webhook.execute(PushEventHandler.handle(payload as never));
-			}
+		if (eventHandler) {
+			await eventHandler(webhookClient, payload);
 		}
-	} catch (error) {
-		if (error instanceof Error) {
-			return setFailed(error.stack ?? error.message);
-		}
-
-		setFailed('Unknown Error');
+	} catch {
+		setFailed('❌ Something went wrong while executing the action [Unknown Error]');
 	}
+})();
+
+function showContextData(gitHubContext: GitHubContext): void {
+	info(`ℹ️ Context Information: ${JSON.stringify(gitHubContext, null, 4)}`);
 }
 
-void run();
-
-type IssueEventMessagesMap = Partial<{
-	[Event in IssuesEvent as Event['action']]: (options: Event) => ContainerBuilder;
-}>;
+type GitHubContext = typeof context;
