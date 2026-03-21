9284d1c7460e4ba7623ae0eb24becf2af530ce19
JJSosaL <251519186+jjsosal@users.noreply.github.com>
2026-03-21 23:41:20 +0100

    refactor: `WebhookClient` now fails when webhook is invalid

--- src/structures/WebhookClient.ts
+++ src/structures/WebhookClient.ts
@@ -1,5 +1,15 @@
+import { info, setFailed } from '@actions/core';
 import type { ContainerBuilder } from '@discordjs/builders';
-import { MessageFlags } from 'discord-api-types/v10';
+import { MessageFlags, RouteBases, Routes } from 'discord-api-types/v10';
+
+const { api } = RouteBases;
+const { webhook } = Routes;
+
+enum HttpStatusCode {
+	NotFound = 404,
+	Ok = 200,
+	Unauthorized = 401,
+}
 
 export class WebhookClient {
 	public readonly webhookId: string;
@@ -10,31 +20,44 @@ export class WebhookClient {
 		this.webhookToken = webhookToken;
 	}
 
-	private createRequestUrl(): URL {
+	private createWebhookExecuteRequest(containerBuilder: ContainerBuilder): Request {
 		const { webhookId, webhookToken } = this;
 
-		const url = new URL(`https://discord.com/api/v10/webhooks/${webhookId}/${webhookToken}`);
-		const { searchParams } = url;
+		const requestUrl = `${api}/${webhook(webhookId, webhookToken)}?with_components=true`;
+		const requestBody = JSON.stringify({
+			components: [
+				containerBuilder,
+			],
+			flags: MessageFlags.IsComponentsV2,
+		});
 
-		searchParams.set('with_components', 'true');
+		const request = new Request(requestUrl, {
+			body: requestBody,
+			method: 'POST',
+		});
 
-		return url;
+		return request;
 	}
 
 	public async execute(containerBuilder: ContainerBuilder): Promise<void> {
-		const url = this.createRequestUrl();
+		const request = this.createWebhookExecuteRequest(containerBuilder);
+		const response = await fetch(request);
 
-		await fetch(url, {
-			body: JSON.stringify({
-				components: [
-					containerBuilder,
-				],
-				flags: MessageFlags.IsComponentsV2,
-			}),
-			headers: {
-				'content-type': 'application/json',
-			},
-			method: 'POST',
-		});
+		const { status } = response;
+
+		switch (status) {
+			case HttpStatusCode.Ok: {
+				return info('Webhook message has been created successfully');
+			}
+			case HttpStatusCode.NotFound: {
+				return setFailed('Webhook has not been found');
+			}
+			case HttpStatusCode.Unauthorized: {
+				return setFailed('Webhook has been unauthorized');
+			}
+			default: {
+				return setFailed('Something went wrong while executing the webhook');
+			}
+		}
 	}
 }
